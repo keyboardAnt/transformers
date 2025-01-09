e10c4b7a708a4283cad41fdbae25dc2f7607278b
jmamou <jonathan.mamou@intel.com>
2025-01-09 03:51:30 -0800

    fix test + style

--- src/transformers/generation/candidate_generator.py
+++ src/transformers/generation/candidate_generator.py
@@ -627,14 +627,14 @@ class AssistantToTargetTranslator:
         self,
         target_tokenizer: "PreTrainedTokenizerBase",
         assistant_tokenizer: "PreTrainedTokenizerBase",
-        assistant_model_device:str = "cpu",
+        assistant_model_device: str = "cpu",
         target_vocab_size: int = None,
         filter_value: float = -float("Inf"),
         suppress_tokens_id: int = -1,
     ):
         self._target_tokenizer: "PreTrainedTokenizerBase" = target_tokenizer
         self._assistant_tokenizer: "PreTrainedTokenizerBase" = assistant_tokenizer
-        self._assistant_model_device:str = assistant_model_device
+        self._assistant_model_device: str = assistant_model_device
         if target_vocab_size:
             self.target_vocab_size: int = target_vocab_size
         else:
@@ -650,20 +650,27 @@ class AssistantToTargetTranslator:
         target_vocab = self._target_tokenizer.get_vocab()
         assistant_vocab = self._assistant_tokenizer.get_vocab()
 
-        target_space_ids = self._target_tokenizer(" ", add_special_tokens=False)["input_ids"]
-        target_space_sign = self._target_tokenizer.convert_ids_to_tokens(target_space_ids)[0][0]
+        space_str = " "
+        target_space_ids = self._target_tokenizer(space_str, add_special_tokens=False)["input_ids"]
+        if len(target_space_ids) > 0:
+            target_space_sign = self._target_tokenizer.convert_ids_to_tokens(target_space_ids)[0][0]
 
-        assistant_space_ids = self._assistant_tokenizer(" ", add_special_tokens=False)["input_ids"]
-        assistant_space_sign = self._assistant_tokenizer.convert_ids_to_tokens(assistant_space_ids)[0][0]
+            assistant_space_ids = self._assistant_tokenizer(space_str, add_special_tokens=False)["input_ids"]
+            if len(assistant_space_ids) > 0:
+                assistant_space_sign = self._assistant_tokenizer.convert_ids_to_tokens(assistant_space_ids)[0][0]
 
-        if target_space_sign != assistant_space_sign:
-            # If the assistant tokenizer has a different space sign than the target tokenizer,
-            # we need to replace the assistant space sign with the target space sign in the assistant_vocab.
-            assistant_vocab = {
-                (tok.replace(assistant_space_sign, target_space_sign, 1) if tok.startswith(assistant_space_sign) else tok): idx
-                for tok, idx in assistant_vocab.items()
-            }
-        
+                if target_space_sign != assistant_space_sign:
+                    # If the assistant tokenizer has a different space sign than the target tokenizer,
+                    # we need to replace the assistant space sign with the target space sign in the assistant_vocab.
+                    assistant_vocab = {
+                        (
+                            tok.replace(assistant_space_sign, target_space_sign, 1)
+                            if tok.startswith(assistant_space_sign)
+                            else tok
+                        ): idx
+                        for tok, idx in assistant_vocab.items()
+                    }
+
         max_assistant_index = max(assistant_vocab.values())
         assistant_to_target_input_ids = torch.full((max_assistant_index + 1,), self.suppress_tokens_id, dtype=int)
         for tok, idx in assistant_vocab.items():
