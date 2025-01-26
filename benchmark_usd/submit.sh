5882fcbd7c242f95b207ba86d349193797474974
Nadav Timor <nadav.timor@weizmann.ac.il>
2025-01-27 01:31:58 +0200

    `module load CUDA/12.4.0`

--- benchmark_usd/submit.sh
+++ benchmark_usd/submit.sh
@@ -5,4 +5,4 @@ timestamp=$(date +\%Y\%m\%d_\%H\%M\%S) && bsub -q "long-gpu short-gpu risk-gpu" 
      -gpu "num=1:mode=exclusive_process:gmem=80000" \
      -oo /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark_out.log \
      -eo /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark_err.log \
-     "module load miniconda/24.11_environmentally && conda activate bench-env && python /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/benchmark.py"
+     "module load miniconda/24.11_environmentally && module load CUDA/12.4.0 && conda activate bench-env && python /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/benchmark.py"
