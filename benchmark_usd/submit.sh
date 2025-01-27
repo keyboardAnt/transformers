4331899315f2c3c1441da2d8d8534379e70f69c0
Nadav Timor <nadav.timor@weizmann.ac.il>
2025-01-28 01:07:34 +0200

    unify lsf output files

--- benchmark_usd/submit.sh
+++ benchmark_usd/submit.sh
@@ -7,6 +7,5 @@ timestamp=$(date +\%Y\%m\%d_\%H\%M\%S) && bsub -q "long-gpu short-gpu risk-gpu" 
     -R "span[hosts=1]" \
     -n 1 \
     -M 200GB \
-    -oo /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark_out.log \
-    -eo /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark_err.log \
+    -o /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark.log \
     "module load miniconda/24.11_environmentally && module load CUDA/12.4.0 && conda activate bench-env && python /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/benchmark.py ${@}"
