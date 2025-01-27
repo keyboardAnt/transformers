219f5803684c1f8ee04c716dfb56eb70b757a2c6
Nadav Timor <nadav.timor@weizmann.ac.il>
2025-01-27 02:54:44 +0200

    lsf: reserve exclusive disk memory & 10 CPU threads (`"affinity[thread*10]"`)

--- benchmark_usd/submit.sh
+++ benchmark_usd/submit.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 timestamp=$(date +\%Y\%m\%d_\%H\%M\%S) && bsub -q "long-gpu short-gpu risk-gpu" \
-     -M 200GB \
-     -gpu "num=1:mode=exclusive_process:gmem=80000" \
-     -oo /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark_out.log \
-     -eo /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark_err.log \
-     "module load miniconda/24.11_environmentally && module load CUDA/12.4.0 && conda activate bench-env && python /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/benchmark.py"
+    -gpu "num=1:j_exclusive=yes:gmem=80GB" \
+    -R "rusage[mem=200GB]" \
+    -R "affinity[thread*10]" \
+    -M 200GB \
+    -oo /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark_out.log \
+    -eo /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark_err.log \
+    "module load miniconda/24.11_environmentally && module load CUDA/12.4.0 && conda activate bench-env && python /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/benchmark.py"
