839182fb9c945493ffbb04c96c0b8c1eb38adbbe
Nadav Timor <nadav.timor@weizmann.ac.il>
2025-01-27 02:59:23 +0200

    bsub -R "affinity[core(8)]" \     -R "span[hosts=1]" \     -n 1 \

--- benchmark_usd/submit.sh
+++ benchmark_usd/submit.sh
@@ -3,7 +3,9 @@
 timestamp=$(date +\%Y\%m\%d_\%H\%M\%S) && bsub -q "long-gpu short-gpu risk-gpu" \
     -gpu "num=1:j_exclusive=yes:gmem=80GB" \
     -R "rusage[mem=200GB]" \
-    -R "affinity[thread*10]" \
+    -R "affinity[core(8)]" \
+    -R "span[hosts=1]" \
+    -n 1 \
     -M 200GB \
     -oo /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark_out.log \
     -eo /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark_err.log \
