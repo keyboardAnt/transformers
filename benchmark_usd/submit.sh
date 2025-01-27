58c43a2b06331e1b8658e214294dc806d9ac9b16
Nadav Timor <nadav.timor@weizmann.ac.il>
2025-01-27 04:18:52 +0200

    fix passing kwargs to script

--- benchmark_usd/submit.sh
+++ benchmark_usd/submit.sh
@@ -9,4 +9,4 @@ timestamp=$(date +\%Y\%m\%d_\%H\%M\%S) && bsub -q "long-gpu short-gpu risk-gpu" 
     -M 200GB \
     -oo /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark_out.log \
     -eo /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/lsf_logs/${timestamp}_jobid_%J_benchmark_err.log \
-    "module load miniconda/24.11_environmentally && module load CUDA/12.4.0 && conda activate bench-env && python /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/benchmark.py \$*"
+    "module load miniconda/24.11_environmentally && module load CUDA/12.4.0 && conda activate bench-env && python /home/projects/dharel/nadavt/repos/transformers/benchmark_usd/benchmark.py ${@}"
